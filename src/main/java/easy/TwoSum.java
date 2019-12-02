62d1be2f376bab70372a4bac67f953c455b75029
Vagif <quliyev_vaqif@icloud.com>
2019-12-02 11:03:03 +0400

    -- Two Sum Solved

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -1,7 +1,27 @@
 package easy;
 
+
+/*
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+
+
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println("dfasdf");
+        int [] nums = {3, 3};
+        int target = 6;
+        int [] result = {0, 1};
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[i]+nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        for (int i : result) System.out.print(i + " ");
     }
 }
